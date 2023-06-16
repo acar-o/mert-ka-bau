c3a7e731c859039a10db13869275753218f9f1e5
oacar <o.acar@propertyexpert.de>
2023-06-16 15:18:48 +0200

    initial commit

---
+++ js/lang.js
@@ -0,0 +1,38 @@
+// Dil seçimini kontrol etmek için localStorage kullanalım
+const selectedLanguage = localStorage.getItem('language');
+
+// Dil seçimi kontrol edilir ve localStorage'a kaydedilir
+if (selectedLanguage) {
+    loadLanguage(selectedLanguage);
+} else {
+    loadLanguage('de'); // Varsayılan olarak Türkçe yüklenir
+}
+
+// Dil seçimi için butonları dinleyelim
+document.getElementById('englishBtn').addEventListener('click', function() {
+    loadLanguage('en');
+});
+
+document.getElementById('turkishBtn').addEventListener('click', function() {
+    loadLanguage('tr');
+});
+
+document.getElementById('germanBtn').addEventListener('click', function() {
+    loadLanguage('de');
+});
+
+// Dil seçildiğinde ilgili dil dosyasını yükleyen fonksiyon
+function loadLanguage(language) {
+    // Dil seçimini localStorage'a kaydedelim
+    localStorage.setItem('language', language);
+    
+    // Dil dosyasını yükleyelim
+    fetch('lang.json')
+        .then(response => response.json())
+        .then(data => {
+            // İlgili dilin karşılıklarını eşleştirelim ve HTML'e yerleştirelim
+            document.getElementById('greeting').textContent = data.greeting[language];
+            document.getElementById('greetingIki').textContent = data.greetingIki[language];
+            document.getElementById('greetingUc').textContent = data.greetingUc[language];
+        });
+}
