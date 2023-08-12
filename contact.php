a3e918c5bc889aa061af2cfd222c073d798b8835
oacar <o.acar@propertyexpert.de>
2023-08-12 18:05:19 +0200

    add

--- contact.php
+++ contact.php
@@ -1,12 +1,12 @@
 <?php
-    if (isset($_POST['submit'])) {
+    if (isset($_POST['submit'])) 
+    {
         $name = $_POST["name"];
         $email = $_POST["email"];
         $subject = $_POST["subject"];
         $message = $_POST["message"];
 
-        $to = "caydenti@gmail.com"; 
-        $subject = "New Contact Form Submission";
+        $to = "info@mertka-bau.de"; 
         $email_content = "Name: $name\n";
         $email_content .= "Email: $email\n";
         $email_content .= "Subject: $subject\n";
@@ -15,6 +15,6 @@
         $headers = "From: $email\r\n";
 
         mail($to, $subject, $email_content, $headers);
-        header("Location: index.html?MessageSent")
+        header("Location: index.html?MessageSent");
     }
 ?>
---
+++ language/datenschutz.js
@@ -0,0 +1,38 @@
+// Dil seçimini kontrol etmek için localStorage kullanalım
+const selectedLanguage = localStorage.getItem("language");
+loadLanguage(selectedLanguage);
+// Dil seçimi için butonları dinleyelim
+document.getElementById("englishBtn").addEventListener("click", function () {
+  loadLanguage("en");
+});
+
+document.getElementById("turkishBtn").addEventListener("click", function () {
+  loadLanguage("tr");
+});
+
+document.getElementById("germanBtn").addEventListener("click", function () {
+  loadLanguage("de");
+});
+
+// Dil seçildiğinde ilgili dil dosyasını yükleyen fonksiyon
+function loadLanguage(language) {
+  // Dil seçimini localStorage'a kaydedelim
+  localStorage.setItem("language", language);
+
+  // Dil dosyasını yükleyelim
+  fetch("language/datenschutz.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // İlgili dilin karşılıklarını eşleştirelim ve HTML'e yerleştirelim
+
+      let keys = Object.keys(data);
+      keys.forEach((element) => {
+        console.log(element);
+      });
+
+      keys.forEach((element) => {
+        document.getElementById(element).textContent = data[element][language];
+        console.log(document.getElementById(element).textContent);
+      });
+    });
+}
